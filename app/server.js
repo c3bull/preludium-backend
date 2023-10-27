e415967a0f7fe9d0d258f884e490a5ce7655d8fb
c3bull <kubcor1@gmail.com>
2023-10-27 18:35:12 +0200

    package update

--- app/server.js
+++ app/server.js
@@ -1,81 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3()
+
 import {graphqlHTTP} from 'express-graphql'
 import mongoose from 'mongoose';
 import schema from './schema';
 
 const app = express();
 const cors = require("cors");
-// store something
-
-app.get('*', async (req, res) => {
-    let filename = req.path.slice(1)
-    await s3.putObject({
-        Body: JSON.stringify({key: "value"}),
-        Bucket: "cyclic-bewildered-tuxedo-hare-eu-west-1",
-        Key: "some_files/my_file.json",
-    })
-
-// get it back
-    let my_file = await s3.getObject({
-        Bucket: "cyclic-bewildered-tuxedo-hare-eu-west-1",
-        Key: "some_files/my_file.json",
-    })
-    try {
-        let s3File = await s3.getObject({
-            Bucket: process.env.BUCKET,
-            Key: filename,
-        })
-
-        res.set('Content-type', s3File.ContentType)
-        res.send(s3File.Body.toString()).end()
-    } catch (error) {
-        if (error.code === 'NoSuchKey') {
-            console.log(`No such key ${filename}`)
-            res.sendStatus(404).end()
-        } else {
-            console.log(error)
-            res.sendStatus(500).end()
-        }
-    }
-})
-
-
-// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
-app.put('*', async (req, res) => {
-    let filename = req.path.slice(1)
-
-    console.log(typeof req.body)
-
-    await s3.putObject({
-        Body: JSON.stringify(req.body),
-        Bucket: process.env.BUCKET,
-        Key: filename,
-    }).promise()
-
-    res.set('Content-type', 'text/plain')
-    res.send('ok').end()
-})
-
-// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
-app.delete('*', async (req, res) => {
-    let filename = req.path.slice(1)
-
-    await s3.deleteObject({
-        Bucket: process.env.BUCKET,
-        Key: filename,
-    }).promise()
-
-    res.set('Content-type', 'text/plain')
-    res.send('ok').end()
-})
-
-// /////////////////////////////////////////////////////////////////////////////
-// Catch all handler for all other request.
-app.use('*', (req, res) => {
-    res.sendStatus(404).end()
-})
 
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
@@ -86,9 +17,6 @@ mongoose.connect(process.env.DATABASE_URL, {
     if (error) {
         console.error(error);
     } else {
-        app.listen(process.env.LISTEN_PORT || 3001, () => {
-            console.log('Listening on port 3001');
-        });
         console.info('Connect with database established');
     }
 });
@@ -106,4 +34,6 @@ app.use('/graphql', graphqlHTTP({
 
 }));
 
-
+app.listen(process.env.LISTEN_PORT, () => {
+    console.log('Listening on port 3001');
+});
