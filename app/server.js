3a7d67eff7a70f14c63208d147537476a4be60c3
c3bull <kubcor1@gmail.com>
2023-10-27 18:24:22 +0200

    package update

--- app/server.js
+++ app/server.js
@@ -13,13 +13,13 @@ app.get('*', async (req, res) => {
     let filename = req.path.slice(1)
     await s3.putObject({
         Body: JSON.stringify({key: "value"}),
-        Bucket: "cyclic-lilac-goat-yoke-eu-west-1",
+        Bucket: "cyclic-bewildered-tuxedo-hare-eu-west-1",
         Key: "some_files/my_file.json",
     })
 
 // get it back
     let my_file = await s3.getObject({
-        Bucket: "cyclic-lilac-goat-yoke-eu-west-1",
+        Bucket: "cyclic-bewildered-tuxedo-hare-eu-west-1",
         Key: "some_files/my_file.json",
     })
     try {
